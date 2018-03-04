727da321f50ad7c3865e3cd88a2c169745e1bf34
Pratik Mulpury <pratik.mulpury@duke.edu>
2018-03-04 17:53:56 -0500

    message

--- trello/javascript/signup.js
+++ trello/javascript/signup.js
@@ -14,12 +14,15 @@ var config = {
     messagingSenderId: "237477910372"
 };
 
+alert('vue  called')
+
 // global access to initialized app database
 var db = firebase.initializeApp(config).database();
 // global reference to remote storage
 var storageRef = firebase.storage().ref();
 // global reference to remote data
 var usersRef = db.ref('Users');
+var projectsRef = db.ref('Projects');
 // connect Firebase to Vue
 Vue.use(VueFire);
 // app Vue instance
@@ -27,67 +30,41 @@ var app = new Vue({
     // app initial state
     data: {
         // user entered data, managed locally before adding to database
+        newImageTitle: '',
+        newUsername: '',
+        newEmail: '',
         newImageTitle: ''
-    },
+        },
 
     // local representations of firebase data
     firebase: {
-        var i, cluster;
-        for (i = 0; i < usersRef.cluster.length; i++)
-        {
-            cluster = usersRef.cluster[i];
-        }
+        users:usersRef,
+        projects:projectsRef
     },
-
-    computed: {
-        // get images in reverse order added
-        reversedImages () {
-            return this.images.slice().reverse();
-        }
-    },
-
     methods: {
-        // get image selected by user and upload it to Firebase for storage
-        storeImage () {
-            // get input element user used to select local image
+        // upload new user to firebase
+        save: function(){
+            alert('save function called')
             var input = document.getElementById('files');
-            // have all fields in the form been completed
-            if (this.newImageTitle && input.files.length > 0) {
+            if (this.newUsername && this.newEmail && this.newImageTitle && input.files.length > 0) {
+                alert('save function if statement called')
                 var file = input.files[0];
-                // get reference to a storage location and
-                storageRef.child('images/' + file.name)
-                          .put(file)
-                          .then(snapshot => this.addImage(this.newImageTitle, snapshot.downloadURL));
-                // reset input values so user knows to input new data
-                input.value = '';
+                // create new user with the username, new email, image title and image
+                storageRef.child('Users/'+newUsername+'/username')
+                .put(newUsername)
+
+                storageRef.child('Users/'+newUsername+'/email')
+                .put(newEmail)
+
+                storageRef.child('Users/'+newUsername+'/images/title')
+                .put(newImageTitle)
+
+                storageRef.child('Users/'+newUsername+'/images/url')
+                .put(file)
+                next('/login.html');
             }
         },
-
-        addImage (title, url) {
-            // now that image has been stored in Firebase, create a reference to it in database
-            imagesRef.push({
-                title: title,
-                url: url
-            });
-            // reset input values so user knows to input new data
-            this.newImageTitle = '';
-        },
-
-        removeImage (img) {
-            if (confirm('Are you sure you want to remove this image?')) {
-                // remove the database entry for the image
-                imagesRef.child(img['.key']).remove();
-                // remove the image in shared storage as well
-                var path = new URL(img.url).pathname.split(/\/|%2F/).slice(-2).join('/');
-                storageRef.child(path)
-                          .delete()
-                          .then(() => console.log('Image deleted'))
-                          .catch(err => console.log(err));
-            }
-        }
     }
-});
-
-
+})
 // mount Vue app within specific HTML element
 app.$mount('#app')
